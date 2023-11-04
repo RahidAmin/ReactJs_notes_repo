d4c07979f67fb8fbecf2e74df82fa404787b3182
RahidAmin <rahidamin19@gmail.com>
2023-11-03 17:21:34 -0700

    cart

--- react-storage-and-handler/src/Components/Cosmetic/Cosmetic.js
+++ react-storage-and-handler/src/Components/Cosmetic/Cosmetic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addToDb } from '../Utilities/fakeDB';
+import { addToDb, removeFromDb } from '../Utilities/fakeDB';
 
 const Cosmetic = (props) => {
    const {pName,price,id}=props.cosmetic;
@@ -14,6 +14,11 @@ const Cosmetic = (props) => {
 //     addToCart(id);
 //    }
 
+   const removeFromCart=id=>
+   {
+    removeFromDb(id);
+   }
+
     return (
         <div style={{margin:"20px",border:"2px solid lightPink",borderRadius:"10px"}}>
             <h3>Buy This:{pName}</h3>
@@ -21,6 +26,7 @@ const Cosmetic = (props) => {
             <h5>ID:{id}</h5>
             {/* <button onClick={addToCartWithParameter}>Add to Cart</button> */}
             <button onClick={()=>addToCart(id)}>Add to Cart</button>
+            <button onClick={()=>removeFromCart(id)}>Remove</button>
         </div>
     );
 };
--- react-storage-and-handler/src/Components/Utilities/fakeDB.js
+++ react-storage-and-handler/src/Components/Utilities/fakeDB.js
@@ -2,15 +2,74 @@
 
 const addToDb=(id)=>
 {
-    const quantity=localStorage.getItem(id);
-    if(quantity)
-    {
-      const newQuantity=parseInt(quantity)+1;
-    //   localStorage.setItem(id,newQuantity);
-    }else
-    {
-        // localStorage.setItem(id,1);
-    }
-}
+  let shoppingCart={}; 
+  
+//get the shopping cart
+  const storedCart=localStorage.getItem('shopping-cart');
+  if(storedCart)
+  {
+     shoppingCart=JSON.parse(storedCart);  //string converts to object
+  }
 
-export{addToDb};
+  //add quantity
+  const quantity=shoppingCart[id];
+  if(quantity)
+  {
+     const newQuantity=quantity+1;
+     shoppingCart[id]=newQuantity;
+  }else{
+      shoppingCart[id]=1;
+  }
+  localStorage.setItem('shopping-cart',JSON.stringify(shoppingCart));
+
+
+
+  
+    // const quantity=localStorage.getItem(id);
+    // if(quantity)
+    // {
+    //   const newQuantity=parseInt(quantity)+1;
+    //    localStorage.setItem(id,newQuantity);
+    // }else
+    // {
+    //     localStorage.setItem(id,1);
+    // }
+}
+const removeFromDb=(id)=>
+{
+  const storedCart=localStorage.getItem('shopping-cart');
+  if(storedCart)
+  {
+    const shoppingCart=JSON.parse(storedCart);   //object
+    if(id in shoppingCart)
+    {
+      delete shoppingCart[id];
+      localStorage.setItem('shopping-cart',JSON.stringify(shoppingCart));
+
+
+       //   const quantity=shoppingCart[id];
+    //   if(quantity)
+    //   {
+    //      const newQuantity=quantity-1;
+    //      shoppingCart[id]=newQuantity;
+    //   }else{
+    //       delete shoppingCart[id];
+    //   }
+    //   localStorage.setItem('shopping-cart',JSON.stringify(shoppingCart));
+    }
+  }
+}
+ 
+
+
+// const deleteShoppingCart=()=>
+// {
+//   localStorage.removeItem("shopping-cart");
+// }
+
+export{
+  addToDb,
+  removeFromDb,
+  // deleteShoppingCart
+}
+        
